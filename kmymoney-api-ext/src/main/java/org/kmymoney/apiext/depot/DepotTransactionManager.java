8fc3870c7b758038e44de9ca9b928de6b7a11f21
James Rossini <james.rossini@posteo.de>
2024-02-19 17:24:55 +0100

    Module kmymoney-api-ext: Initial project structure

---
+++ kmymoney-api-ext/src/main/java/org/kmymoney/apiext/depot/DepotTransactionManager.java
@@ -0,0 +1,10 @@
+package org.kmymoney.apiext.depot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DepotTransactionManager {
+    // Logger
+    private static final Logger LOGGER = LoggerFactory.getLogger(DepotTransactionManager.class);
+
+}
