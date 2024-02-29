971415551ab3cc812c548e7bf26e462f1cb83234
James Rossini <james.rossini@posteo.de>
2024-02-29 13:58:35 +0100

    Small correction for read-only-part of user-defined attributes: Better symmetry with sister project.

--- kmymoney-api/src/main/java/org/kmymoney/api/read/impl/hlp/HasUserDefinedAttributesImpl.java
+++ kmymoney-api/src/main/java/org/kmymoney/api/read/impl/hlp/HasUserDefinedAttributesImpl.java
@@ -3,6 +3,9 @@ package org.kmymoney.api.read.impl.hlp;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.kmymoney.api.Const;
+import org.kmymoney.api.generated.KEYVALUEPAIRS;
+import org.kmymoney.api.generated.ObjectFactory;
 import org.kmymoney.api.generated.PAIR;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,8 +17,30 @@ public class HasUserDefinedAttributesImpl // implements HasUserDefinedAttributes
 
 	// ---------------------------------------------------------------
 
+	public static String getUserDefinedAttributeCore(final KEYVALUEPAIRS kvps, final String name) {
+		if ( kvps == null )
+			return null;
+		
+		return getUserDefinedAttributeCore(kvps.getPAIR(), name);
+	}
+	
+	public static List<String> getUserDefinedAttributeKeysCore(final KEYVALUEPAIRS kvps) {
+		if ( kvps == null )
+			return null;
+		
+		return getUserDefinedAttributeKeysCore(kvps.getPAIR());
+	}
+    
+	// ---------------------------------------------------------------
+
     public static String getUserDefinedAttributeCore(final List<PAIR> kvpList, 
     		                                         final String name) {
+		if ( kvpList == null )
+			return null;
+
+		if ( name.equals("") )
+			return null;
+		
 		if ( ! getUserDefinedAttributeKeysCore(kvpList).contains(name) ) {
 			throw new KVPListDoesNotContainKeyException();
 		}
@@ -30,6 +55,9 @@ public class HasUserDefinedAttributesImpl // implements HasUserDefinedAttributes
     }
 
     public static List<String> getUserDefinedAttributeKeysCore(final List<PAIR> kvpList) {
+		if ( kvpList == null )
+			return null;
+		
 		List<String> retval = new ArrayList<String>();
 
 		for ( PAIR kvp : kvpList ) {
@@ -37,6 +65,69 @@ public class HasUserDefinedAttributesImpl // implements HasUserDefinedAttributes
 		}
 
 		return retval;
+	}
+
+    
+	// ---------------------------------------------------------------
+
+	// Return KVPs without the ones with dummy content
+	public static List<PAIR> getKVPListClean(final List<PAIR> kvpList) {
+		List<PAIR> retval = new ArrayList<PAIR>();
+
+		for ( PAIR kvp : kvpList ) {
+			if ( ! kvp.getKey().equals(Const.KVP_KEY_DUMMY) ) {
+				retval.add(kvp);
+			}
+		}
+
+		return retval;
+	}
+
+	// Remove KVPs with dummy content
+	public static void cleanKVPs(final List<PAIR> kvpList) {
+		for ( PAIR kvp : kvpList ) {
+			if ( ! kvp.getKey().equals(Const.KVP_KEY_DUMMY) ) {
+				kvpList.remove(kvp);
+			}
+		}
+	}
+	
+	// ---------------------------------------------------------------
+	
+	// Sic, not in HasWritableUserDefinedAttributes
+	//                ========
+	public static void setKVPsInit(
+			KEYVALUEPAIRS currKVPs,
+			final KEYVALUEPAIRS newKVPs) {
+		if ( newKVPs == null ) {
+			throw new IllegalArgumentException("null 'kvps' given!");
+		}
+
+		KEYVALUEPAIRS oldKVPs = currKVPs;
+		if ( oldKVPs == newKVPs ) {
+			return; // nothing has changed
+		}
+		// ::TODO Check with equals as well
+		currKVPs = newKVPs;
+
+		// we have an xsd-problem saving empty KVPs so we add a dummy-value
+		if ( newKVPs.getPAIR().isEmpty() ) {
+			ObjectFactory objectFactory = new ObjectFactory();
+
+			KEYVALUEPAIRS kvps = objectFactory.createKEYVALUEPAIRS();
+
+			PAIR newKVP = objectFactory.createPAIR();
+			newKVP.setKey(Const.KVP_KEY_DUMMY);
+			newKVP.setValue(Const.KVP_KEY_DUMMY);
+
+			newKVPs.getPAIR().add(newKVP);
+		}
+
+		// <<insert code to react further to this change here
+//		PropertyChangeSupport ptyChgFirer = myPtyChg;
+//		if ( ptyChgFirer != null ) {
+//			ptyChgFirer.firePropertyChange("kvps", oldKVPs, newKVPs);
+//		}
 	}
 
 }
