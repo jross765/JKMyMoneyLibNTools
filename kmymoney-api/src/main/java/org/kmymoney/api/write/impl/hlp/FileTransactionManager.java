73a07cf5bcbcd15492daa02610944c81ee20f25c
James Rossini <james.rossini@posteo.de>
2024-05-08 09:12:13 +0200

    Module "API", write-branch: FileTransactionManager: added overwrite-method createTransactionSplit().
    Cf. sister project, commit dd34adff683fbad95e20878fa9620deed6b4befe.

--- kmymoney-api/src/main/java/org/kmymoney/api/write/impl/hlp/FileTransactionManager.java
+++ kmymoney-api/src/main/java/org/kmymoney/api/write/impl/hlp/FileTransactionManager.java
@@ -1,9 +1,14 @@
 package org.kmymoney.api.write.impl.hlp;
 
+import org.kmymoney.api.generated.SPLIT;
 import org.kmymoney.api.generated.TRANSACTION;
+import org.kmymoney.api.read.KMyMoneyTransaction;
 import org.kmymoney.api.read.impl.KMyMoneyTransactionImpl;
+import org.kmymoney.api.read.impl.KMyMoneyTransactionSplitImpl;
+import org.kmymoney.api.write.KMyMoneyWritableTransaction;
 import org.kmymoney.api.write.impl.KMyMoneyWritableFileImpl;
 import org.kmymoney.api.write.impl.KMyMoneyWritableTransactionImpl;
+import org.kmymoney.api.write.impl.KMyMoneyWritableTransactionSplitImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,17 +34,21 @@ public class FileTransactionManager extends org.kmymoney.api.read.impl.hlp.FileT
 		return trx;
 	}
 
-	// ::TODO
-//    @Override
-//    protected KMyMoneyTransactionSplitImpl createTransactionSplit(
-//	    final GncTransaction.TrnSplits.TrnSplit jwsdpTrxSplt,
-//	    final KMyMoneyTransaction trx,
-//	    final boolean addSpltToAcct,
-//	    final boolean addSpltToInvc) {
-//	KMyMoneyWritableTransactionSplitImpl splt = new KMyMoneyWritableTransactionSplitImpl(jwsdpTrxSplt, trx, 
-//                								           addSpltToAcct, addSpltToInvc);
-//	LOGGER.debug("createTransactionSplit: Generated new writable transaction split: " + splt.getID());
-//	return splt;
-//    }
+    @Override
+	protected KMyMoneyTransactionSplitImpl createTransactionSplit(
+			final SPLIT jwsdpTrxSplt,
+			final KMyMoneyTransaction trx, // actually, should be KMyMoney*Writable*Transaction, 
+			                               // but then the compiler is not happy...
+			final boolean addSpltToAcct) {
+    	if ( ! ( trx instanceof KMyMoneyWritableTransaction ) ) {
+    		throw new IllegalArgumentException("transaction must be a writable one");
+    	}
+    	
+    	KMyMoneyWritableTransactionSplitImpl splt = new KMyMoneyWritableTransactionSplitImpl(jwsdpTrxSplt, 
+    																						 (KMyMoneyWritableTransaction) trx, 
+                								           									 addSpltToAcct);
+    	LOGGER.debug("createTransactionSplit: Generated new writable transaction split: " + splt.getID());
+    	return splt;
+    }
 
 }
